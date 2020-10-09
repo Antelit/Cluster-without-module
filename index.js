1e5bd7a59ac2f1125729bfb217e7237f3f4fd706
Antelit <ishiboshi@gmail.com>
2020-10-09 14:48:34 +0200

    final Web

--- index.js
+++ index.js
@@ -1,126 +1,139 @@
 const fs = require('fs');
 const redis = require("redis");
-const client = redis.createClient();
+const spawn = require('child_process').spawn;
 const child_process = require('child_process');
+
+let SETTING =  JSON.parse(fs.readFileSync('Setting.json'));
+
 const WebSocketServer = new require('ws');
-var spawn = require('child_process').spawn;    
-const port = 7000
+const PORT = SETTING.WebSocket.Port
+const WEBSOCKETDEBUG = SETTING.WebSocket.Debug;
 
+const PIDs = SETTING.Redis.keyWorkProcess;
+const MAIN_NODE = SETTING.Redis.keyMainNode;
+const SUBSCRIBE_KEY = SETTING.Redis.keySubscribe;
+const DEADMASTERS_KEY = SETTING.Redis.keyDeadMaster;
+const DEADSLAVES_KEY = SETTING.Redis.keyDeadSlave;
 
-const PIDs = "PID"
-const MAIN_NODE = "MAIN_NODE"
-const PREF_SL_NODE = 'Node_'
+const PREF_SL_NODE = SETTING.Node.Prefix_Node;
+const COUNT_NODES =  SETTING.Node.Count;
+const NODEDEBUG = SETTING.Node.Debug;
 
-let clients = {};
-client.flushall();
-client.subscribe('pubsub'); 
+const client = redis.createClient();        // Client for work on redis
+const subscriber = redis.createClient();    // Client on subscribe event
 
-client.on("message", function(error,data) {
+let clients = {};                       // List WebSocket Client
+
+client.flushall();                      // Clear Redis DB
+subscriber.subscribe(SUBSCRIBE_KEY);    // Listen Event on key
+
+// Send message to all WebSocketClient
+subscriber.on("message", function(error,data) {
     for (var key in clients) {
         clients[key].send(data);
     }
-    //console.log("!!!!MSG " + data);
 });
 
 client.on("error", function(error) {
-    console.error("ERROR!!!!"  + error);
+    console.error("ERROR!!!! "  + error);
 });
-
-for(var i = 0; i < 6; i++) {
-    var worker_process = child_process.fork("worker.js", [i]); 
-    console.log(worker_process.pid)
-    worker_process.on('message', function (msg) {
-        console.log("!!!!!!!" + msg.PID);
-    });
-    
+// Start Workers
+for(var i = 0; i < COUNT_NODES; i++) {
+    var worker_process = child_process.fork("worker.js"); 
+    //Listen Event 'close' on Worker
     worker_process.on('close', function (code,q,w) {
         CloseProcess(this)
     });
 }
 
 function CloseProcess(proc){
-    console.log("Process " + proc.pid + " is DEAD!")
+    if(NODEDEBUG)
+        console.log(`Process ${proc.pid} is DEAD!`) 
+
     json = { "type": "Slave", "status": "NodeCrash", "Node": PREF_SL_NODE + proc.pid }
     
     for (var key in clients) {
         clients[key].send(JSON.stringify(json));
     }
 
-    client.unsubscribe('pubsub', function (err, done ) {
-        client.LREM(PIDs, 1, proc.pid);
-        client.get(MAIN_NODE, function(err, reply) {
-            if(reply == proc.pid){
-                client.lrange(PIDs, 0, -1, function(err, socketIds) {
-                    changeMainWork(socketIds[0])
-                });
-            }
-            client.subscribe('pubsub')
-        })
+    client.LREM(PIDs, 1, proc.pid); // Delete DEAD process from list work process
+    
+    client.get(MAIN_NODE, function(err, reply) {
+        if(reply == proc.pid){ // If dead is main process
+            client.lpush(DEADMASTERS_KEY, proc.pid);
+            client.lrange(PIDs, 0, -1, function(err, PIDS) {
+                if(NODEDEBUG)
+                    console.log(`Process ${proc.pid} was the MASTER!`) 
+                let getRandom = randomInt(0, PIDS.length -1) // getRandom for find work node and set master
+                changeMainWork(PIDS[getRandom])
+            });
+        }
+        else{
+            client.lpush(DEADSLAVES_KEY, proc.pid);
+        }
     })
-    
-    
-    var worker_process = child_process.fork("worker.js", [i]); 
-    worker_process.on('close', function (code,q,w) {
+
+    var worker_process = child_process.fork("worker.js"); // Start new node 
+    //Listen Event 'close' on Worker
+    worker_process.on('close', function () {
         CloseProcess(this)
     });
-
-    
 }
 
 function changeMainWork(PID){
-    client.unsubscribe('pubsub', function (err, done ) {
-        client.set(MAIN_NODE, PID, function(err, data){
-            console.log(err, data)
-        });
-        client.subscribe('pubsub')
-    })
+    //Set new MASTER NODE
+    if(NODEDEBUG)
+        console.log(`Process ${PID} now Master!`) 
+    client.set(MAIN_NODE, PID, function(err){
+        if(err)
+            console.log(err)
+    });
 }
-
-let webSocketServer = new WebSocketServer.Server({port: port}, 
+//Start WebSocket Server
+let webSocketServer = new WebSocketServer.Server({port: PORT}, 
     function (){
-        console.log(`WebSocket server Run! On port: ${port}`)
+        console.log(`WebSocket server Run! On port: ${PORT}`)
     }
 );
 
-webSocketServer.on('connection', function(ws) {
+webSocketServer.on('connection', function(WebSocketClient) {
+    //Save client WebSocket
     var id = Math.random();
-    clients[id] = ws;
+    if(WEBSOCKETDEBUG)
+        console.log(`WebSocket connect new client ${id}`) 
+    clients[id] = WebSocketClient;
+    // Create INIT data to new client WebSocket
+    client.get(MAIN_NODE, function(err, reply){ 
+        let MASTER_NODE = reply
 
-    ws.send("Hello");
-    client.unsubscribe('pubsub', function (err, done ) {
-        client.get(MAIN_NODE, function(err, reply){ 
-            let MASTER_NODE = reply
-
-            client.lrange(PIDs, 0, -1, function(err, socketIds) {
-                socketIds.slice(MASTER_NODE,1)
-                
-                let RunSlave = new Array();
-
-                for (let index = 0; index < socketIds.length; index++) {
-                    if(socketIds[index] != MASTER_NODE)
-                        RunSlave.push(socketIds[index])
-                }
-                let js = {
-                    "type":"init", 
-                    "Master": {"Run" : MASTER_NODE, "Dead":[]},
-                    "Slave": {"Run": RunSlave, "Dead":[]},
-                }
-                //console.log(JSON.stringify(js))
-                ws.send(JSON.stringify(js));
-                client.subscribe('pubsub')
-            });
-
+        client.lrange(PIDs, 0, -1, function(err, PIDS) {
+            let RunSlave = new Array();
+            for (let index = 0; index < PIDS.length; index++) {
+                if(PIDS[index] != MASTER_NODE)
+                    RunSlave.push(PIDS[index])
+            }
+            client.lrange(DEADMASTERS_KEY, 0, -1, function(err, D_Masters) {
+                client.lrange(DEADSLAVES_KEY, 0, -1, function(err, D_Slaves) {
+                    let js = {"type":"init", "Master": {"Run" : MASTER_NODE, "Dead":D_Masters},"Slave": {"Run": RunSlave, "Dead":D_Slaves}}
+                    WebSocketClient.send(JSON.stringify(js));
+                })
+            })
             
-        })
-    });
+        });
+    })
     
-    ws.on('message', function(message){ onMessage(message) });
+    WebSocketClient.on('message', function(message){ onMessage(message) });
 
-    ws.on('close', function() { onClose(id) });
-
+    WebSocketClient.on('close', function() { onClose(id) });
 });
 
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+//Listen incomint message on WebSocket
 function onMessage(Message){
+    if(WEBSOCKETDEBUG)
+        console.log(Message)
     try{
         let msg = JSON.parse(Message)
         switch(msg.action){
@@ -137,16 +150,18 @@ function onMessage(Message){
 }
 function StopProcess(pid){
     switch(process.platform) {
-        case 'win32':
+        case 'win32': //On Win32-64
             spawn("taskkill", ["/pid", pid, '/f', '/t']);
         break;
-        case 'darwin':
+        case 'darwin': // On MacOs
             spawn("kill", [pid]);
-        default:
+        default: // Other?
+            spawn("kill", [pid]);
             console.log(process.platform)
     }
 }
 function onClose(ID_Client){
-    console.log('Connection Close ' + ID_Client);
+    if(WEBSOCKETDEBUG)
+        console.log(`WebSocket Client ${ID_Client} disconect`) 
     delete clients[ID_Client];
 }
--- worker.js
+++ worker.js
@@ -1,54 +1,59 @@
+const fs = require('fs');
 const redis = require("redis");
 const client = redis.createClient();
-const PIDs = "PID"
-const MAIN_NODE = "MAIN_NODE"
-const RNDINTs = "RNDINTs"
-const PREF_SL_NODE = 'Node_'
-const MasterIntervalMS = 100
-const SlaveIntervalMS = 500
-let idInterval
 
+let SETTING =  JSON.parse(fs.readFileSync('Setting.json'));
+
+const publisher = redis.createClient();
+
+const PIDs = SETTING.Redis.keyWorkProcess;
+const MAIN_NODE = SETTING.Redis.keyMainNode;
+const RNDINTs = SETTING.Redis.keyQueue;
+const SUBSCRIBE_KEY =SETTING.Redis.keySubscribe;
+
+const PREF_SL_NODE = SETTING.Node.Prefix_Node;
+const NODEDEBUG = SETTING.Node.Debug;
+const MasterIntervalMS = SETTING.Node.MasterInterval;
+const SlaveIntervalMS = SETTING.Node.SlaveInterval;
+const MoreCrashe = SETTING.Node.MoreCrasheSlave;
+
+let idInterval
+//Check main_node on start worker
 client.get(MAIN_NODE, function(err, reply) {
     let json
     if(!reply){
+        // Main Node is null
+        // set main node
         client.set(MAIN_NODE, process.pid)
+        //Set Action Main
         idInterval = setInterval(IntervalMaster, MasterIntervalMS);
-        json = {
-            "type": "Master",
-            "status": "Create",
-            "Node" : PREF_SL_NODE + process.pid
-            }
+        json = {"type": "Master", "status": "Create", "Node" : PREF_SL_NODE + process.pid}
     }else{
+        // Have Main_Node
+        // I`m Slave
+        //Set Action Slave
         idInterval = setInterval(IntervalSlave, SlaveIntervalMS)
-        json = {
-            "type": "Slave",
-            "status": "Create",
-            "Node" : PREF_SL_NODE + process.pid
-            }
+        json = { "type": "Slave", "status": "Create", "Node" : PREF_SL_NODE + process.pid}
     }
-    client.publish("pubsub", JSON.stringify(json))
+    publisher.publish(SUBSCRIBE_KEY, JSON.stringify(json))
 });
-
-client.lpush(PIDs, process.pid, function(err) {
-    console.log('New Node ' + process.pid);
-});
-
-let lastID = 0
+//Add Work Process
+client.lpush(PIDs, process.pid);
 
 function IntervalMaster(){
     client.get(MAIN_NODE, function(err, reply) {
-        if(reply == process.pid){
-            client.lpush(RNDINTs, Math.random());
+        if(reply == process.pid){ // Check: Change Master?
+            client.lpush(RNDINTs, Math.random()); // Add on queue random float
+            client.lrange(RNDINTs, 0, -1, function(err, cnt_int) {
+                t = { "type": "Master", "status": "QueueWait", "Count" : cnt_int.length}
+                publisher.publish(SUBSCRIBE_KEY, JSON.stringify(t))
+            })
         }
-        else{
-            clearInterval(idInterval)
-            t = {
-                "type": "Master",
-                "status": "BecameSlave",
-                "Node" : PREF_SL_NODE + process.pid
-                }
-            client.publish("pubsub", JSON.stringify( t) )
-
+        else{ //I'm not the boss anymore
+            clearInterval(idInterval) // stop master interval
+            t = { "type": "Master", "status": "BecameSlave", "Node" : PREF_SL_NODE + process.pid}
+            publisher.publish(SUBSCRIBE_KEY, JSON.stringify(t))
+            // Set interval on Slave
             idInterval =  setInterval(IntervalSlave, SlaveIntervalMS)
         }
     });
@@ -56,55 +61,41 @@ function IntervalMaster(){
 
 function IntervalSlave(){
     client.get(MAIN_NODE, function(err, reply) {
-        if(reply == process.pid){
-            console.log("PID " +  process.pid + " Now i`m Main " )
-            clearInterval(idInterval)
+        // Chack: maybe I'm master?
+        if(reply == process.pid){ // YES!
+            clearInterval(idInterval) // stop slave interval
+            // Set interval on Master
             idInterval = setInterval(IntervalMaster, MasterIntervalMS);
-            t = {
-                "type": "Master",
-                "status": "Change",
-                "Node" : PREF_SL_NODE + process.pid
-                }
-            client.publish("pubsub", JSON.stringify( t) )
+            t = { "type": "Master", "status": "Change","Node" : PREF_SL_NODE + process.pid}
+            publisher.publish(SUBSCRIBE_KEY, JSON.stringify( t) )
         }
-        else{
+        else{ // NO:(
+            //Get queue
             client.lrange(RNDINTs, 0, -1, function(err, cnt_int) {
-                if(cnt_int.length){
-                    
-                    let id = randomIntFromInterval(0, cnt_int.length)
-                    let lastvar = cnt_int[id]
+                if(cnt_int.length){ // Check queue
+                    let lastvar = cnt_int[randomInt(0, (cnt_int.length * ((MoreCrashe) ? 1.1 : 1))-1)] // get random value from queue
                     if(lastvar){
-                        client.LREM(RNDINTs, 1, lastvar);
+                        client.LREM(RNDINTs, 1, lastvar); //Delete value from queue
                         
-                        client.get(PREF_SL_NODE + process.pid, function(err, reply) {
+                        client.get(PREF_SL_NODE + process.pid, function(err, reply) { // get count processed
                             let cnt;
-                            if(!reply)
-                                cnt = 1
-                            else
-                                cnt = parseInt(reply) + 1
-                                //client.set(PREF_SL_NODE + process.pid, parseInt(reply) + 1)  
-                            client.set(PREF_SL_NODE + process.pid, cnt)  
+                            cnt = (!reply) ? 1 : parseInt(reply) + 1
+
+                            client.set(PREF_SL_NODE + process.pid, cnt)  // set new processed
                             
                             let json = {
-                                "type": "Slave",
-                                "status": "ActionComplite",
-                                "Node" : PREF_SL_NODE + process.pid,
-                                "ActionValue": lastvar,
-                                "Processed": cnt
+                                    "type": "Slave", "status": "ActionComplite", "Node" : PREF_SL_NODE + process.pid, 
+                                    "ActionValue": lastvar,"Processed": cnt
                                 }
                             //Send to WS on Action DONE!
-                            client.publish("pubsub", JSON.stringify(json))
+                            publisher.publish(SUBSCRIBE_KEY, JSON.stringify(json))
                         })
-                        console.log("PID " + process.pid + "; queue " + cnt_int.length + "; action " + lastvar)
+                        if(NODEDEBUG)
+                            console.log("PID " + process.pid + "; queue " + cnt_int.length + "; action " + lastvar)
                     }
                     else{
-                        let json = {
-                            "type": "Slave",
-                            "status": "NodeCrash",
-                            "Node": PREF_SL_NODE + process.pid
-                            }
-                        client.publish("pubsub", JSON.stringify(json))
-                        //console.log("PID " + process.pid + " !!!CRASH!!!")
+                        let json = {"type": "Slave","status": "NodeCrash","Node": PREF_SL_NODE + process.pid}
+                        publisher.publish(SUBSCRIBE_KEY, JSON.stringify(json))
                         process.exit(1)
                     }
                 }
@@ -112,7 +103,7 @@ function IntervalSlave(){
         }
     })
 }
-function randomIntFromInterval(min, max) { // min and max included 
+function randomInt(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 process.on('message', function (msg) {
